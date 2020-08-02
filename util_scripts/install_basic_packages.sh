016dac13f1571e7ed7fae7afca5f3eefbc8557ed
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2020-08-02 18:59:27 -0300

    Removida a instalacao de pacotes desnecessarios

--- util_scripts/install_basic_packages.sh
+++ util_scripts/install_basic_packages.sh
@@ -2,18 +2,11 @@
 apt-get update
 apt-get upgrade -y
 apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
     curl \
-    gnupg-agent \
-    software-properties-common \
     python3-pip \
     wget \
     make \
     nano \
-    gpg \
-    python-gpg \
-    python-certbot-nginx \
     htop
 pip3 install mako
 pip3 install click
